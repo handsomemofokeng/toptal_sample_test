09287060bde4a1e1ad13a762f3fca7ad6b8ff0b7
Handsome Mofokeng <handsomem23@hotmail.com>
2021-02-19 18:30:11 +0200

    * .gitignore: initial comit
    * toptal_sample_test.sln:
    * Program.cs:
    * toptal_sample_test.csproj:

---
+++ toptal_sample_test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace toptal_sample_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int missingInt =1;
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+
+            return missingInt;
+        }
+    }
+}
